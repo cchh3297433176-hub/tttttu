c814fc5d2ff8a3b820afe8cbf39390b19fe22d7b
海盗船长 <cchh3297433176@gmail.com>
2026-09-20 00:51:13 +0800

    Add files via upload

--- assets/stickers/小狗/list.js
+++ assets/stickers/小狗/list.js
@@ -1,1 +1,114 @@
+/**
+ * assets/stickers/小狗/list.js
+ * 🐶 【小狗】表情包分组独立注册清单（94 张 seolgi 小狗动图 GIF）
+ */
+(function() {
+    'use strict';
 
+    if (typeof window.registerStickerPack !== 'function') {
+        window._MCYT_PENDING_STICKERS = window._MCYT_PENDING_STICKERS || {};
+        window._MCYT_PENDING_STICKERS['小狗'] = [];
+    }
+
+    const list = [
+        { desc: '仰泳晃动手臂冒爱心', url: 'https://pic1.imgdb.cn/item/69c0a15eccd26bacb4daa6a7.gif' },
+        { desc: '快速激动奔跑', url: 'https://pic1.imgdb.cn/item/69c0a15eccd26bacb4daa6ab.gif' },
+        { desc: '伸舌头抬右手晃尾巴', url: 'https://pic1.imgdb.cn/item/69c0a308ccd26bacb4daa6bb.gif' },
+        { desc: '黄狗快乐唱歌扭动', url: 'https://pic1.imgdb.cn/item/69c0a308ccd26bacb4daa6bd.gif' },
+        { desc: '白狗头顶冒红心摇晃', url: 'https://pic1.imgdb.cn/item/69c0a497ccd26bacb4daa6d2.gif' },
+        { desc: '黄狗头顶冒红心摇晃', url: 'https://pic1.imgdb.cn/item/69c0a647ccd26bacb4daa6f3.gif' },
+        { desc: '白狗盖被趴枕头乱想', url: 'https://pic1.imgdb.cn/item/69c0a497ccd26bacb4daa6d3.gif' },
+        { desc: '黄狗盖被趴枕头乱想', url: 'https://pic1.imgdb.cn/item/69c0a647ccd26bacb4daa6f6.gif' },
+        { desc: '白狗包里掏出巨大爱心', url: 'https://pic1.imgdb.cn/item/69c0a497ccd26bacb4daa6d6.gif' },
+        { desc: '黄狗包里掏出巨大爱心', url: 'https://pic1.imgdb.cn/item/69c0a67fccd26bacb4daa6fa.gif' },
+        { desc: '白狗抽泣委屈', url: 'https://pic1.imgdb.cn/item/69c0a497ccd26bacb4daa6d4.gif' },
+        { desc: '黄狗抽泣委屈', url: 'https://pic1.imgdb.cn/item/69c0b275ccd26bacb4dabf21.gif' },
+        { desc: '白狗双眼冒爱心摇尾巴', url: 'https://pic1.imgdb.cn/item/69c0a5f3ccd26bacb4daa6e8.gif' },
+        { desc: '黄狗迷恋爱心', url: 'https://pic1.imgdb.cn/item/69c0a647ccd26bacb4daa6f1.gif' },
+        { desc: '白狗哭着抱大药丸', url: 'https://pic1.imgdb.cn/item/69c0a5f3ccd26bacb4daa6ec.gif' },
+        { desc: '黄狗哭着抱大药丸', url: 'https://pic1.imgdb.cn/item/69c0b496ccd26bacb4dabf51.gif' },
+        { desc: '白狗托起小黄狗', url: 'https://pic1.imgdb.cn/item/69c0a5f3ccd26bacb4daa6ed.gif' },
+        { desc: '黄狗托起小白狗', url: 'https://pic1.imgdb.cn/item/69c0a647ccd26bacb4daa6f2.gif' },
+        { desc: '白狗穿婚纱拿手捧花', url: 'https://pic1.imgdb.cn/item/69c0a5f3ccd26bacb4daa6eb.gif' },
+        { desc: '白狗香蕉装闪星星', url: 'https://pic1.imgdb.cn/item/69c0a5f3ccd26bacb4daa6ea.gif' },
+        { desc: '黄狗香蕉装闪星星', url: 'https://pic1.imgdb.cn/item/69c0a67fccd26bacb4daa6f9.gif' },
+        { desc: '黄狗打电脑白狗贴背', url: 'https://pic1.imgdb.cn/item/69c0a497ccd26bacb4daa6d7.gif' },
+        { desc: '白狗打电脑黄狗贴背', url: 'https://pic1.imgdb.cn/item/69c0b275ccd26bacb4dabf20.gif' },
+        { desc: '黄狗嘟嘴亲亲冒爱心', url: 'https://pic1.imgdb.cn/item/69c0b275ccd26bacb4dabf23.gif' },
+        { desc: '趴在身上充电', url: 'https://pic1.imgdb.cn/item/69c0b275ccd26bacb4dabf22.gif' },
+        { desc: '白狗穿睡衣靠枕头', url: 'https://pic1.imgdb.cn/item/69c0a647ccd26bacb4daa6f4.gif' },
+        { desc: '拿出背后棉花糖', url: 'https://pic1.imgdb.cn/item/69c0b5e9ccd26bacb4dabfa6.gif' },
+        { desc: '享受阳光闭眼照耀', url: 'https://pic1.imgdb.cn/item/69c0b5e9ccd26bacb4dabfa7.gif' },
+        { desc: '小狗被窝睡觉守候', url: 'https://pic1.imgdb.cn/item/69c0b5e9ccd26bacb4dabfa9.gif' },
+        { desc: '趴背亲吻捏脸颊', url: 'https://pic1.imgdb.cn/item/69c0b5e9ccd26bacb4dabfab.gif' },
+        { desc: '盖绿被蹭大红心', url: 'https://pic1.imgdb.cn/item/69c0b5e9ccd26bacb4dabfa8.gif' },
+        { desc: '抱手机等消息', url: 'https://pic1.imgdb.cn/item/69c0b5e9ccd26bacb4dabfaa.gif' },
+        { desc: '裹绿被墙后偷看', url: 'https://pic1.imgdb.cn/item/69c0b718ccd26bacb4dac170.gif' },
+        { desc: '抱腿悬挂不放手', url: 'https://pic1.imgdb.cn/item/69c0b718ccd26bacb4dac16f.gif' },
+        { desc: '包包里惊喜钻出', url: 'https://pic1.imgdb.cn/item/69c0b718ccd26bacb4dac172.gif' },
+        { desc: '黄狗戴蓝手套点赞', url: 'https://pic1.imgdb.cn/item/69c0b718ccd26bacb4dac173.gif' },
+        { desc: '白狗戴黄手套点赞', url: 'https://pic1.imgdb.cn/item/69c0bd6945b603369a3d9b78.gif' },
+        { desc: '关灯被窝看手机', url: 'https://pic1.imgdb.cn/item/69c0b718ccd26bacb4dac174.gif' },
+        { desc: '紫耳朵黄狗一脸坏笑', url: 'https://pic1.imgdb.cn/item/69c0b718ccd26bacb4dac175.gif' },
+        { desc: '坐黄垫灵光一闪', url: 'https://pic1.imgdb.cn/item/69c0b7d0ccd26bacb4dac4b0.gif' },
+        { desc: '趴床边哭泣委屈', url: 'https://pic1.imgdb.cn/item/69c0b7d0ccd26bacb4dac4ae.gif' },
+        { desc: '主动钻进购物车', url: 'https://pic1.imgdb.cn/item/69c0b7d0ccd26bacb4dac4af.gif' },
+        { desc: '烟囱里举望远镜观察', url: 'https://pic1.imgdb.cn/item/69c0b7d0ccd26bacb4dac4b1.gif' },
+        { desc: '托脸颊冒星星', url: 'https://pic1.imgdb.cn/item/69c0b7d0ccd26bacb4dac4ad.gif' },
+        { desc: '互相牵绳遛狗', url: 'https://pic1.imgdb.cn/item/69c0b90bccd26bacb4dacb4e.gif' },
+        { desc: '趴着抬头傻笑摇尾', url: 'https://pic1.imgdb.cn/item/69c0b90bccd26bacb4dacb4a.gif' },
+        { desc: '突然受到惊吓', url: 'https://pic1.imgdb.cn/item/69c0b90bccd26bacb4dacb49.gif' },
+        { desc: '抱着小熊蹭脸蛋', url: 'https://pic1.imgdb.cn/item/69c0b90bccd26bacb4dacb48.gif' },
+        { desc: '趴屏幕前一脸期待', url: 'https://pic1.imgdb.cn/item/69c0b90bccd26bacb4dacb4d.gif' },
+        { desc: '鼻孔喷气满脸期待', url: 'https://pic1.imgdb.cn/item/69c0b90bccd26bacb4dacb4b.gif' },
+        { desc: '抱黄色枕头舒坦', url: 'https://pic1.imgdb.cn/item/69c0ba49ccd26bacb4dad1e4.gif' },
+        { desc: '双手散开冒大粉心', url: 'https://pic1.imgdb.cn/item/69c0ba49ccd26bacb4dad1e7.gif' },
+        { desc: '两只小狗转圈追逐', url: 'https://pic1.imgdb.cn/item/69c0ba49ccd26bacb4dad1e5.gif' },
+        { desc: '拿小木棍往前走', url: 'https://pic1.imgdb.cn/item/69c0ba49ccd26bacb4dad1e6.gif' },
+        { desc: '戴苹果头套委屈', url: 'https://pic1.imgdb.cn/item/69c0ba49ccd26bacb4dad1e8.gif' },
+        { desc: '1号粉丝牌开心摇晃', url: 'https://pic1.imgdb.cn/item/69c0ba49ccd26bacb4dad1e9.gif' },
+        { desc: '屁股上盖OK印章', url: 'https://pic1.imgdb.cn/item/69c0bc9445b603369a3d9726.gif' },
+        { desc: '拉拉手开心跳舞', url: 'https://pic1.imgdb.cn/item/69c0bc9445b603369a3d9727.gif' },
+        { desc: '疯狂发射粉色爱心', url: 'https://pic1.imgdb.cn/item/69c0bc9445b603369a3d9728.gif' },
+        { desc: '黄狗伴舞开心跳跃', url: 'https://pic1.imgdb.cn/item/69c0bc9545b603369a3d972a.gif' },
+        { desc: '白狗伴舞开心跳跃', url: 'https://pic1.imgdb.cn/item/69c0c60f45b603369a3da304.gif' },
+        { desc: '拿小戳子戳生气狗', url: 'https://pic1.imgdb.cn/item/69c0bc9545b603369a3d972b.gif' },
+        { desc: '网兜一网捞出爱心', url: 'https://pic1.imgdb.cn/item/69c0bc9545b603369a3d972c.gif' },
+        { desc: '乖乖坐着冒爱心', url: 'https://pic1.imgdb.cn/item/69c0bd6945b603369a3d9b7a.gif' },
+        { desc: '拼命用力点头赞同', url: 'https://pic1.imgdb.cn/item/69c0bd8845b603369a3d9c1e.gif' },
+        { desc: '信件里源源不断冒爱心', url: 'https://pic1.imgdb.cn/item/69c0c1eb45b603369a3da2ca.gif' },
+        { desc: '背后发条累瘫在地', url: 'https://pic1.imgdb.cn/item/69c0c1eb45b603369a3da2cb.gif' },
+        { desc: '认真给同伴捶背', url: 'https://pic1.imgdb.cn/item/69c0c1eb45b603369a3da2ce.gif' },
+        { desc: '被子里眼神迷糊迷茫', url: 'https://pic1.imgdb.cn/item/69c0c1eb45b603369a3da2cc.gif' },
+        { desc: '扭屁股晃尾巴跳舞', url: 'https://pic1.imgdb.cn/item/69c0c1eb45b603369a3da2cf.gif' },
+        { desc: '脏兮兮坐地认错', url: 'https://pic1.imgdb.cn/item/69c0c24d45b603369a3da2d2.gif' },
+        { desc: '死死抱腿蹭腿撒娇', url: 'https://pic1.imgdb.cn/item/69c0c24e45b603369a3da2d4.gif' },
+        { desc: '揉揉没睡醒的眼睛', url: 'https://pic1.imgdb.cn/item/69c0c46d45b603369a3da2f0.gif' },
+        { desc: '趴玩具火箭飞天', url: 'https://pic1.imgdb.cn/item/69c0c46d45b603369a3da2eb.gif' },
+        { desc: '从地底下突然冒出', url: 'https://pic1.imgdb.cn/item/69c0c51b45b603369a3da2f8.gif' },
+        { desc: '站在梯子上拿喇叭喊', url: 'https://pic1.imgdb.cn/item/69c0c51b45b603369a3da2f9.gif' },
+        { desc: '躺靠枕满脸幸福惬意', url: 'https://pic1.imgdb.cn/item/69c0c51b45b603369a3da2fa.gif' },
+        { desc: '推开窗双手招手打招呼', url: 'https://pic1.imgdb.cn/item/69c0c51b45b603369a3da2fc.gif' },
+        { desc: '可怜兮兮泪眼汪汪', url: 'https://pic1.imgdb.cn/item/69c0c51b45b603369a3da2fd.gif' },
+        { desc: '震惊到骨头从嘴里滑落', url: 'https://pic1.imgdb.cn/item/69c0c60f45b603369a3da303.gif' },
+        { desc: '拿粉刷粉刷墙壁', url: 'https://pic1.imgdb.cn/item/69c0c60f45b603369a3da306.gif' },
+        { desc: '躺被窝里准备睡觉', url: 'https://pic1.imgdb.cn/item/69c0c6cd45b603369a3da308.gif' },
+        { desc: '假装玩小车实则竖耳偷听', url: 'https://pic1.imgdb.cn/item/69c0c6cd45b603369a3da30a.gif' },
+        { desc: '耳朵里藏礼物露出得意', url: 'https://pic1.imgdb.cn/item/69c0c6cd45b603369a3da30d.gif' },
+        { desc: '坐报纸上风吹可怜小狗', url: 'https://pic1.imgdb.cn/item/69c0c6cd45b603369a3da30c.gif' },
+        { desc: '裹被子里探手招招', url: 'https://pic1.imgdb.cn/item/69c0c7d845b603369a3da332.gif' },
+        { desc: '盖小被子瑟瑟发抖', url: 'https://pic1.imgdb.cn/item/69c0c7d945b603369a3da334.gif' },
+        { desc: '一脸坏笑望过来', url: 'https://pic1.imgdb.cn/item/69c0c7d945b603369a3da335.gif' },
+        { desc: '四肢伸缩做有氧健美操', url: 'https://pic1.imgdb.cn/item/69c0c7d945b603369a3da333.gif' },
+        { desc: '站在体重秤上拽肥肉发愁', url: 'https://pic1.imgdb.cn/item/69c0c8ad45b603369a3dadb7.gif' },
+        { desc: '伸爪爪扭屁股做拉伸', url: 'https://pic1.imgdb.cn/item/69c0c8ad45b603369a3dadb8.gif' },
+        { desc: '黄狗被揉捏胖脸蛋', url: 'https://pic1.imgdb.cn/item/69c0c8ad45b603369a3dadb9.gif' },
+        { desc: '白狗被揉捏胖脸蛋', url: 'https://pic1.imgdb.cn/item/69c0c8ad45b603369a3dadba.gif' }
+    ];
+
+    if (typeof window.registerStickerPack === 'function') {
+        window.registerStickerPack('小狗', list);
+    } else {
+        window._MCYT_PENDING_STICKERS['小狗'] = list;
+    }
+})();
