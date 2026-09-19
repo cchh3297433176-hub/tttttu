bf2a3b1fb56cc47b8676fe160ab8712c44650763
海盗船长 <cchh3297433176@gmail.com>
2026-09-20 00:48:24 +0800

    Add files via upload

--- assets/stickers/豆米乌卡/list.js
+++ assets/stickers/豆米乌卡/list.js
@@ -1,1 +1,61 @@
+/**
+ * assets/stickers/豆米乌卡/list.js
+ * 🐱🐶 【豆米乌卡】表情包分组独立注册清单（共 40 张）
+ */
+(function() {
+    'use strict';
 
+    if (typeof window.registerStickerPack !== 'function') {
+        window._MCYT_PENDING_STICKERS = window._MCYT_PENDING_STICKERS || {};
+        window._MCYT_PENDING_STICKERS['豆米乌卡'] = [];
+    }
+
+    const list = [
+        { desc: '头顶加载思索', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005348_origin_mmexport1784611626132.png' },
+        { desc: '委屈放声大哭', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005350_origin_mmexport1784611626517.png' },
+        { desc: '双手合十祈求', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005351_origin_mmexport1784611626710.png' },
+        { desc: '消息已收到', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005342_origin_mmexport1784611624409.png' },
+        { desc: '相拥依偎贴贴', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005341_origin_mmexport1784611624165.png' },
+        { desc: '举钻戒求婚示爱', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005340_origin_mmexport1784611624026.png' },
+        { desc: '爱心环绕委屈祈求', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005347_origin_mmexport1784611625901.png' },
+        { desc: '头顶着火不爽', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005345_origin_mmexport1784611625273.png' },
+        { desc: '挥拳冒火愤怒', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005346_origin_mmexport1784611625567.png' },
+        { desc: '抱着大爱心愉悦', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005344_origin_mmexport1784611625020.png' },
+        { desc: '感动落泪爱意包围', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005343_origin_mmexport1784611624770.png' },
+        { desc: '戴睡帽困倦犯困', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005335_origin_mmexport1784611622974.png' },
+        { desc: '戴墨镜耍酷闪耀', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005334_origin_mmexport1784611622741.png' },
+        { desc: '举禁止牌表示拒绝', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005339_origin_mmexport1784611623864.png' },
+        { desc: '遗憾离场落寞退场', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005338_origin_mmexport1784611623708.png' },
+        { desc: '比爱心眨眼放电', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005337_origin_mmexport1784611623486.png' },
+        { desc: '眼神亮晶晶害羞欲言', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005379_origin_a5cae49601a8ecfa.png' },
+        { desc: '满眼星光合十期待', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005336_origin_mmexport1784611623207.png' },
+        { desc: '手拿绿色对勾标牌', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005349_origin_mmexport1784611626344.png' },
+        { desc: '高举双手欢呼点赞', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005381_origin_mmexport1784626203895.png' },
+        { desc: '独自抽烟落寞倦怠', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005383_origin_mmexport1784626560473.png' },
+        { desc: '闭眼大喊激动宣泄', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005382_origin_mmexport1784626559987.png' },
+        { desc: '严肃推眼镜认真思索', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005384_origin_mmexport1784626560987.png' },
+        { desc: '看手机心碎落泪', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005385_origin_mmexport1784626561526.png' },
+        { desc: '小猫小狗甜蜜依偎', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005386_origin_mmexport1784626562108.png' },
+        { desc: '揪住脖颈气呼呼', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005394_origin_mmexport1784626571220.png' },
+        { desc: '对手指脸红腼腆害羞', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005392_origin_mmexport1784626569050.png' },
+        { desc: '推眼镜托下巴沉思', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005393_origin_mmexport1784626569854.png' },
+        { desc: '单手擦眼泪抽泣', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005391_origin_mmexport1784626567817.png' },
+        { desc: '气泡打招呼Hi', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005390_origin_mmexport1784626566804.png' },
+        { desc: '手拿菜刀乖巧威胁', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005389_origin_mmexport1784626565663.png' },
+        { desc: '伸手指向一旁气恼', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005388_origin_mmexport1784626564974.png' },
+        { desc: '闭眼浅笑惬意得意', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005387_origin_mmexport1784626563645.png' },
+        { desc: '端杯子疑惑问号', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005396_origin_mmexport1784626573539.png' },
+        { desc: '弯眼笑开口道谢谢谢', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005401_origin_mmexport1784626576502.png' },
+        { desc: '大颗泪珠崩溃大哭', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005399_origin_mmexport1784626575458.png' },
+        { desc: '佩戴领结浪漫送花', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005400_origin_mmexport1784626575975.png' },
+        { desc: '额头怒气抬手发火', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005397_origin_mmexport1784626574167.png' },
+        { desc: '星光自信得意比手势', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005398_origin_mmexport1784626574804.png' },
+        { desc: '趴手机等对方消息', url: 'https://gitee.com/zqn1013/gitee/raw/master/img/1000005395_origin_mmexport1784626572001.png' }
+    ];
+
+    if (typeof window.registerStickerPack === 'function') {
+        window.registerStickerPack('豆米乌卡', list);
+    } else {
+        window._MCYT_PENDING_STICKERS['豆米乌卡'] = list;
+    }
+})();
